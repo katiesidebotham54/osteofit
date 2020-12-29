5dc7e4f3d2b1a7a386846a0032e8da09bf069d65
Ben Sidebotham <Bensidebotham89@gmail.com>
2020-12-29 16:38:32 -0500

    homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,16 +3,19 @@ import { Paper } from "@material-ui/core";
 import Autumn from "../images/automn.jpeg";
 import Path from "../images/path.jpeg";
 import Tree from "../images/tree.jpeg";
+import Product from "../images/productBlue1.png"
+/*import '../mysass.scss';*/
+
 class Home extends Component {
   render() {
     return (
       <div>
         <div className="Mission">
-          <Paper className="MissionText">
-            <h2>Accuracy</h2>
-            <h2>Function</h2>
-            <h2>Sterility</h2>
-          </Paper>
+          <div class="MissionText">
+            <h2 className = "a">Accurate</h2>
+            <h2 className = "b">Sterile</h2>
+            <h2 className = "c">Effective</h2>
+          </div>
           <div class="slideshow-container">
             <div class="slideshow-inner">
               <div class="mySlides fade">
@@ -33,11 +36,31 @@ class Home extends Component {
             <span class="dot" onclick="currentSlide(3)"></span>
           </div>
         </div>
-        <div class="home-arrow">
+        {/*<div class="home-arrow">
           <div class="chevron"></div>
           <div class="chevron"></div>
           <div class="chevron"></div>
           <span class="text">Scroll down</span>
+    </div>*/}
+        <div className = "HomePara">
+          <p>OsteoFit is a medical device company focused on the <b>development</b> of cost <b>effective</b> orthopedic instruments
+             which address <b>accuracy</b>, <b>function</b> and <b>sterility</b> in a routine and <b>sustainable</b> process for hospitals.</p>
+        </div>
+        <div className = "item-3d">
+          <span class="ground"></span>
+          <figure class="item-content group">
+            <div class="item-img">
+              <img src={Product} alt="" />
+            </div>
+            <figcaption class="item-caption">
+              <p>
+              </p>
+              <p>
+              </p>
+              <p>
+              </p>
+            </figcaption>
+          </figure>
         </div>
       </div>
     );
