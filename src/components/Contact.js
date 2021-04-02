aad4bdcc2fcdb85d45e217c0b503059684fb749c
Ben Sidebotham <Bensidebotham89@gmail.com>
2021-04-01 20:02:43 -0400

    nav bar

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,35 +7,54 @@ import OsteofitLogoOnly from '../images/OsteofitLogoOnly.png'
 
 
 const Contact = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
-
- const handleSubmit = (e) => {
-  e.preventDefault();
-  db.collection('contacts').add({
-    name: name,
-    email: email,
-    subject: subject,
-    message: message
-  })
-  .then(() => {
-    alert('Message has been submitted!');
-  })
-  .catch(error => {
-    alert(error.message);
-  });
-
-  setName('')
-  setEmail('')
-  setSubject('')
-  setMessage('')
-
- }
 
   return (
-    <div className="Contact">
+      <body>
+    <title>contact form</title>
+
+    <link href="contact-form.css" rel="stylesheet"></link>
+
+    <div class="fcf-body">
+
+    <div id="fcf-form">
+    <h3 class="fcf-h3">Contact us</h3>
+
+    <form id="fcf-form-id" class="fcf-form-class" method="post" action="contact-form-process.php">
+        
+        <div class="fcf-form-group">
+            <label for="Name" class="fcf-label">Your name</label>
+            <div class="fcf-input-group">
+                <input type="text" id="Name" name="Name" class="fcf-form-control" required></input>
+            </div>
+        </div>
+
+        <div class="fcf-form-group">
+            <label for="Email" class="fcf-label">Your email address</label>
+            <div class="fcf-input-group">
+                <input type="email" id="Email" name="Email" class="fcf-form-control" required></input>
+            </div>
+        </div>
+
+        <div class="fcf-form-group">
+            <label for="Message" class="fcf-label">Your message</label>
+            <div class="fcf-input-group">
+                <textarea id="Message" name="Message" class="fcf-form-control" rows="6" maxlength="3000" required></textarea>
+            </div>
+        </div>
+
+        <div class="fcf-form-group">
+            <button type="submit" id="fcf-button" class="fcf-btn fcf-btn-primary fcf-btn-lg fcf-btn-block">Send Message</button>
+        </div>
+
+        <div class="fcf-credit" id="fcf-credit">
+        Simple HTML email form provided by: <a href="https://www.freecontactform.com" target="_blank">FreeContactForm.com</a>
+        </div>
+    </form>
+    </div>
+      </div>
+    </body>
+  )
+    {/*<div className="Contact">
       <h1 className = "title">
           <span className="fade-in-text">Contact Us</span>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
@@ -121,8 +140,7 @@ const Contact = () => {
       </form>
       </div>
     </div>
-    </div>
-  );
-};
+  </div>*/}
+}
 
 export default Contact;
