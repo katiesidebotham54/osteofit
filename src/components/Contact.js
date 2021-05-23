386a82bff240b62a9b08534c8629d3a65ab9daed
Katie Sidebotham <katiesidebotham54@gmail.com>
2021-05-22 22:12:03 -0400

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,50 +1,131 @@
-import React from "react";
+import React, { useState } from 'react'
+import axios from 'axios'
 
-
-const Contact = () => {
-
-  return (
-     <div>
-    <title>contact form</title>
-
-    <link href="contact-form.css" rel="stylesheet"></link>
-
-    <div class="fcf-body">
-
-    <div id="fcf-form">
-    <h3 class="fcf-h3">Contact us</h3>
-
-    <form id="fcf-form-id" class="fcf-form-class" method="post" action="contact-form-process.php">
-        
-        <div class="fcf-form-group">
-            <label for="Name" class="fcf-label">Your name</label>
-            <div class="fcf-input-group">
-                <input type="text" id="Name" name="Name" class="fcf-form-control" required></input>
-            </div>
-        </div>
-
-        <div class="fcf-form-group">
-            <label for="Email" class="fcf-label">Your email address</label>
-            <div class="fcf-input-group">
-                <input type="email" id="Email" name="Email" class="fcf-form-control" required></input>
-            </div>
-        </div>
-
-        <div class="fcf-form-group">
-            <label for="Message" class="fcf-label">Your message</label>
-            <div class="fcf-input-group">
-                <textarea id="Message" name="Message" class="fcf-form-control" rows="6" maxlength="3000" required></textarea>
-            </div>
-        </div>
-
-        <div class="fcf-form-group">
-            <button type="submit" id="fcf-button" class="fcf-btn fcf-btn-primary fcf-btn-lg fcf-btn-block">Send Message</button>
-        </div>
-
-    </form>
-    </div>
-      </div>
-      </div>
-  )
+export default () => {
+  const [status, setStatus] = useState({
+    submitted: false,
+    submitting: false,
+    info: { error: false, msg: null },
+  })
+  const [inputs, setInputs] = useState({
+    firstName:'',
+    lastName:'',
+    subject:'',
+    email: '',
+    message: '',
+  })
+  const handleServerResponse = (ok, msg) => {
+    if (ok) {
+      setStatus({
+        submitted: true,
+        submitting: false,
+        info: { error: false, msg: msg },
+      })
+      setInputs({
+        firstName:'',
+        lastName:'',
+        subject:'',
+        email: '',
+        message: '',
+      })
+    } else {
+      setStatus({
+        info: { error: true, msg: msg },
+      })
+    }
   }
-export default Contact;
+  const handleOnChange = (e) => {
+    e.persist()
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }))
+    setStatus({
+      submitted: false,
+      submitting: false,
+      info: { error: false, msg: null },
+    })
+  }
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    setStatus((prevStatus) => ({ ...prevStatus, submitting: true }))
+    axios({
+      method: 'POST',
+      url: 'https://formspree.io/f/mvodkepk',
+      data: inputs,
+    })
+      .then((response) => {
+        handleServerResponse(
+          true,
+          'Thank you, your message has been submitted.'
+        )
+      })
+      .catch((error) => {
+        handleServerResponse(false, error.response.data.error)
+      })
+  }
+  return (
+    <main>
+      <h1>Get in Touch!</h1>
+      <hr />
+      <form onSubmit={handleOnSubmit}>
+      <label htmlFor="first_name">First Name</label>
+        <input
+          id="first_name"
+          type="text"
+          name="firstName"
+          onChange={handleOnChange}
+          required
+          value={inputs.firstName}
+        />
+      <label htmlFor="last_name">Last Name</label>
+        <input
+          id="last"
+          type="text"
+          name="lastName"
+          onChange={handleOnChange}
+          required
+          value={inputs.lastName}
+        />
+      <label htmlFor="subject">Subject</label>
+        <input
+          id="subject"
+          type="text"
+          name="_subject"
+          onChange={handleOnChange}
+          required
+          value={inputs.subject}
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          name="_replyto"
+          onChange={handleOnChange}
+          required
+          value={inputs.email}
+        />
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          name="message"
+          onChange={handleOnChange}
+          required
+          value={inputs.message}
+        />
+        <button type="submit" disabled={status.submitting}>
+          {!status.submitting
+            ? !status.submitted
+              ? 'Submit'
+              : 'Submitted'
+            : 'Submitting...'}
+        </button>
+      </form>
+      {status.info.error && (
+        <div className="error">Error: {status.info.msg}</div>
+      )}
+      {!status.info.error && status.info.msg && <p>{status.info.msg}</p>}
+    </main>
+  )
+}
