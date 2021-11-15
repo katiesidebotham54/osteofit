d05b5d781c24d03196e854241830396f54edfbff
Katie Sidebotham <katiesidebotham54@gmail.com>
2021-11-15 00:41:42 -0500

    patent change

--- src/documents/Patent8556.js
+++ src/documents/Patent8556.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Patent9101368 from '../documents/9101368.pdf';
+import Patent8556897 from '../documents/US8556897.pdf';
 
 
 const Patent9101 = () => {
     return(
         <div>
-            <embed type = "application/pdf" width="100%" height="500px" src={Patent9101368}/>
+            <embed type = "application/pdf" width="100%" height="500px" src={Patent8556897}/>
         </div>
     )
 }
